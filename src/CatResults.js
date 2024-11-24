b09f38ba86abc72f3c399ab526d3811625564bf4
Danny Cui <cui_chong@hotmail.com>
2024-11-25 10:47:05 +1100

    extract expected result

--- src/CatResults.js
+++ src/CatResults.js
@@ -1,10 +1,26 @@
 const CatResults = ({ facts }) => {
-  return facts.length > 0 && (
-    <ul>
-      {facts.map((fact) => (
-        <li>{fact}</li>
-      ))}
-    </ul>
+  let facts = facts
+    .filter((fact) => fact.deleted != null)
+    .map((fact) => {
+      fact.contents = fact.contents.replace(/&quot;/g, '"');
+      return fact;
+    })
+    .reduce((acc, curFact) => {
+      if (curFact.extended) {
+        return acc;
+      }
+
+      return acc.concat(curr);
+    }, []);
+
+  return (
+    facts.length > 0 && (
+      <ul>
+        {facts.map((fact) => (
+          <li>{fact.contents}</li>
+        ))}
+      </ul>
+    )
   );
 };
 
